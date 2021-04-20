060029136c6c547d5537202a60cc600fcb0ad82d
Anonymous <Anonymous@DESKTOP-HOEEBFT>
2021-04-20 21:27:20 +0800

    Test App

---
+++ js/components/questions.js
@@ -0,0 +1,501 @@
+const questions = [
+    {
+        question: "What is computer Network Components?",
+        choice1: "Equipment gadget that splits the organization association between different gadgets.",
+        choice2: "Embrace each physical elements and also the software system needed to put in pc networks in each organizations and houses.",
+        choice3: "That offer even as get administrations from totally different companions in a very workgroup network",
+        choice4: "A gadget that adds 3G or 4G (LTE) cell network to PCs, PCs and different tablets.",
+        answer: 2
+    },
+    {
+        question: "What is Cable and Connectors mean?",
+        choice1: "is a transmission media utilized for communicating a sign.",
+        choice2: "is an equipment gadget that permits the PC to associate with the web over the current phone line.", 
+        choice3: "an equipment gadget which is utilized to associate a LAN with a web association.",
+        choice4: "is an equipment gadget that associates different gadgets on a PC organization.",
+        answer: 1
+    },
+    {
+        question: "What is Router mean?",
+        choice1: "is a transmission media utilized for communicating a sign.", 
+        choice2: "is an equipment gadget that permits the PC to associate with the web over the current phone line.",
+        choice3: "an equipment gadget which is utilized to associate a LAN with a web association.", 
+        choice4: "is an equipment gadget that associates different gadgets on a PC organization.",
+        answer: 3
+    },
+    {
+        question: "What is Switch mean?",
+        choice1: "an equipment gadget which is utilized to associate a LAN with a web.",
+        choice2: "is an equipment gadget that permits the PC to associate with the web over the current phone line.", 
+        choice3: "is a transmission media utilized for communicating a sign.",
+        choice4: "is an equipment gadget that associates different gadgets on a PC organization.",
+        answer: 4
+    },
+    {
+        question: "What is the advantage of Router?",
+        choice1: "Causes you to share printers, scanners, and email.",
+        choice2: "Causes you to share data at exceptionally quick speed.", 
+        choice3: "Security: The data which is communicated to the organization will navigate the whole link, yet the lone determined gadget which has been tended to can peruse the information.",
+        choice4: "Electronic correspondence is more productive and more affordable than without the network.",
+        answer: 3
+    },
+    {
+        question: "What is Modem?",
+        choice1: "is an equipment gadget that permits the PC to associate with the web over the current phone line.",
+        choice2: "is a transmission media utilized for communicating a sign.", 
+        choice3: "is an equipment gadget that associates different gadgets on a PC organization.",
+        choice4: "Electronic correspondence is more productive and more affordable than without the network.",
+        answer: 1
+    },
+    {
+        question: "What are the two types of NIC?",
+        choice1: "Cables and Connectors",
+        choice2: "Switch and Router", 
+        choice3: "Wired and Wireless",
+        choice4: "Hub and Modem",
+        answer: 3
+    },
+]
+
+
+const question2 = [ 
+    {
+        type: "multiple choice",
+        question: "What is acronym for LAN?",
+        choice1: "Local Area Networking",
+        choice2: "Local Area Networking",
+        choice3: "Local Access Network",
+        choice4: "Local  Area Network",
+        answer: 4
+    },
+    {
+        type: "multiple choice",
+        question: "Define what is LAN?",
+        choice1: "A private network dependent on the internet protocol",
+        choice2: "A group of computers connected to each other in a small area",
+        choice3: "A network organized within an individual person",
+        choice4: "A network that reaches out over an enormous geological area",
+        answer: 2
+    },
+    {
+        type: "multiple choice",
+        question: " What is acronym MAN stands for?",
+        choice1: "Metropolitan Access Network",
+        choice2: "Multi-Area Network",
+        choice3: "Metropolitan Area Network",
+        choice4: "Metropolitan Area Net",
+        answer: 3
+    },
+    {
+        type: "multiple choice",
+        question: "What are the three common types of Computer Networks?",
+        choice1: "RAM, LAN, WAN",
+        choice2: "MAN, BAN, WAN",
+        choice3: "MAN, LAN, WAN",
+        choice4: "None of the Above",
+        answer: 3
+    },
+    {
+        type: "multiple choice",
+        question: "What are the types of Internetwork?",
+        choice1: "Ultranet and Internet",
+        choice2: "Intranet and Ultranet",
+        choice3: "Extranet and Intranet",
+        choice4: "Extranet and Internet",
+        answer: 3
+    },
+    {
+        type: "multiple choice",
+        question: "Who is the scientist researcher that brings the idea of PAN?",
+        choice1: "Marcus Zimmerman",
+        choice2: "Thomas Zimmerman",
+        choice3: "Thomas Edison",
+        choice4: "George Zimmerman",
+        answer: 2
+    },
+    {
+        type: "multiple choice",
+        question: "One of the largest Wide Area network?",
+        choice1: "Last Mile",
+        choice2: "Intranet",
+        choice3: "Internet",
+        choice4: "Mobile Broadband",
+        answer: 3
+    },
+    {
+        type: "multiple choice",
+        question: "These are the Advantages of Wide Area Network?",
+        choice1: "High Bandwidth,  Get updated files, Geographical Area",
+        choice2: "High Bandwidth, Sharing of software and resources, Time saving",
+        choice3: "Geographical Area, Time saving, High Bandwidth",
+        choice4: "High Bandwidth, Collaboration, Communication",
+        answer: 1
+    },
+    {
+        type: "true-or-false",
+        question: "Wide Area Network is larger than the LAN?",
+        choice1: "True",
+        choice2: "False",
+        answer: 1
+    },
+    {
+        type: "true-or-false",
+        question: "Types of Network are categories by their Geographical Area Cover?",
+        choice1: "True",
+        choice2: "False",
+        answer: 1
+    },
+]
+
+
+const questions3 = [ 
+    {
+        question: "In TCP/IP model this layer is responsible for message segmentation, flow control and correction that establishes basic data channels that application, use for task-specific data exchange. What called in this layer?",
+        choice1: "Link layer",
+        choice2: "Application layer",
+        choice3: "Transport layer",
+        choice4: "Internet Layer",
+        answer: 3
+    },
+
+    {
+        question: "What layer in OSI model that accountable for the transmission and reception of unstructured information between a tool and a physical transmission medium?",
+        choice1: "Data layer",
+        choice2: "Application layer",
+        choice3: "Transport layer",
+        choice4: "Physical Layer",
+        answer: 4
+    },
+
+    {
+        question: "It is accountable for the character code translation, conversion, compression, and cryptography. What called this layer in OSI Model?",
+        choice1: "Application layer",
+        choice2: "Transport layer ",
+        choice3: "Presentation layer",
+        choice4: "Session Layer",
+        answer: 3
+    },
+    
+    {
+        question: "What layer combine the physical and data link layers where functions into a single layer. Including all hosts accessible without traversing a router?",
+        choice1: "Application layer",
+        choice2: "Internet layer",
+        choice3: "Session Layer",
+        choice4: "Link Layer",
+        answer: 4
+    }
+]
+
+const questions4 = [
+    {
+        title: "DNS and History",
+        question: "What is Domain Name System",
+        choice1: "Programming",
+        choice2: "Computer and Network Service",
+        choice3: "Internet Protocol",
+        choice4: "Hardware",
+        answer: 2
+    },
+    {
+        title: "DNS and History",
+        question: "Who Invented the Domain Name System?",
+        choice1: "Charles A Babbage",
+        choice2: "Ray Noorda",
+        choice3: "Paul Mockapetris",
+        choice4: "Ada Lovelace",
+        answer: 3
+    },
+    {
+        title: "DNS and History",
+        question: "When was Domain Name System created?",
+        choice1: "1983",
+        choice2: "1985",
+        choice3: "1981",
+        choice4: "1984",
+        answer: 1
+    },
+    {
+        title: "DNS and History",
+        question: "What is DNSSEC?",
+        choice1: "it is a security expansion",
+        choice2: "in the realm of the Internet",
+        choice3: "in this situation, the assailant sends Domain Name System queries for a few irregular",
+        choice4: "is a fundamental piece of the framework of the internet",
+        answer: 1
+    },
+
+]
+
+const questions5 = [
+    {
+        title: "Functions and Configurations",
+        question: "What function that sets retry time and break?",
+        choice1: "NBT Cache",    
+        choice2: "Server",    
+        choice3: "Cache",    
+        choice4: "Resolver",
+        choice5: "Proxy",
+        answer: 4    
+    },
+    {
+        title: "Functions and Configurations",
+        question: "What function that showcases the cache data?",
+        choice1: "Server",    
+        choice2: "Resolver",    
+        choice3: "NBT Cache",    
+        choice4: "Proxy",
+        choice5: "Cache",
+        answer: 3    
+    },
+    {
+        title: "Functions and Configurations",
+        question: "Configure DNS ___________and default domain names for the security machine. What is the answer?",
+        choice1: "Server",    
+        choice2: "NBT Cache",    
+        choice3: "Resolver",    
+        choice4: "Proxy",
+        choice5: "Cache",
+        answer: 1    
+    },
+    {
+        title: "Functions and Configurations",
+        question: "What function that store DNS mappings to reserve to accelerate question?",
+        choice1: "Server",    
+        choice2: "NBT Cache",    
+        choice3: "Resolver",    
+        choice4: "Proxy",
+        choice5: "Cache",
+        answer: 5    
+    },  
+    {
+        title: "Functions and Configurations",
+        question: "What is retry and break in resolver configuration incorporates?",
+        choice1: "Obtains from DNS reaction and Adds DNS mappings to cache physically",    
+        choice2: "If the DNS demand isn't reacted after timeout and System will sit tight for DNS server's reaction subsequent to sending the DNS demand.",    
+        choice3: "In the DNS Cache Configuration dialog, configure options and Click OK to save your settings.",    
+        choice4: "On the Task tab in the right auxiliary pane, click DNS and On the Cache tab in the DNS List dialog, click New.",
+        choice5: "Obtains from DNS reaction and Adds DNS mappings to cache physically.",
+        answer: 2  
+    },  
+]
+
+const questions6 = [
+    {
+        title: "DNS Process Works",
+        question: "Our PC will at that point search for the IP address related with the domain name in its located DNS cache. What process is this?",
+        choice1: "Contact the Recursive DNS Servers",
+        choice2: "Query the Authoritative DNS Servers",
+        choice3: "Mentioning Website Information",
+        choice4: "Final process",
+        answer: 3
+    },
+    {
+        title: "DNS Process Works",
+        question: "A Recursive DNS server gets to the Address record for www.sololearn.com from the authoritative name servers. What process is this?",
+        choice1: "Final process",
+        choice2: "Mentioning Website Information",
+        choice3: "Contact the Recursive DNS Servers",
+        choice4: "Query the Authoritative DNS Servers",
+        answer: 4
+    },
+    {
+        title: "DNS Process Works",
+        question: "What process that talks about numerous ISP's utilization similar recursive DNS servers, it's conceivable that regular domain name is as of now in its cache?",
+        choice1: "Contact the Recursive DNS Servers",
+        choice2: "Query the Authoritative DNS Servers",
+        choice3: "Final process",
+        choice4: "Mentioning Website Information",
+        answer: 1
+    },
+    {
+        title: "DNS Process Works",
+        question: "This process explains the internet browser will interface with the web server related with the Address records IP and show the site. What is this?",
+        choice1: "Query the Authoritative DNS Servers",
+        choice2: "Final process",
+        choice3: "Mentioning Website Information",
+        choice4: "Contact the Recursive DNS Servers",
+        answer: 2
+    },
+    {
+        title: "DNS Process Works",
+        question: "How many steps do those DNS processes have?",
+        choice1: "5 steps",
+        choice2: "6 steps",
+        choice3: "4 steps",
+        choice4: "8 steps",
+        answer: 3
+    },
+]
+
+const questions7 = [
+    {
+        title: "Name Space",
+        question: "It is utilized for planning a location to a name. What it is?",
+        choice1: "Inverse Domain",
+        choice2: "Generic Domain",
+        choice3: "Domain Name",
+        choice4: "Country Domain",
+        answer: 1
+    },
+    {
+        title: "Name Space",
+        question: "What is characterizes the enlisted has as per their conventional behavior?",
+        choice1: "Inverse Domain",
+        choice2: "Generic Domain",
+        choice3: "Domain Name",
+        choice4: "Country Domain",
+        answer: 4
+    },
+    {
+        title: "Name Space",
+        question: "What is the level of Cooperates business organizations and Businesses of firms?",
+        choice1: "com and pro",
+        choice2: "coop and biz",
+        choice3: "aero and biz",
+        choice4: "org and coop",
+        answer: 3
+    },
+    {
+        title: "Name Space",
+        question: "The arrangement of country area is same as a nonexclusive space. What name space is this?",
+        choice1: "Generic Domain",
+        choice2: "Inverse Domain",
+        choice3: "Domain Name",
+        choice4: "Country Domain",
+        answer: 4
+    },
+    {
+        title: "Name Space",
+        question: "Domain __________________  name administration gave by the Internet to Transmission Control Protocol organizations/Internet Protocol (TCP/IP). What is the answer?",
+        choice1: "Generic Domain",
+        choice2: "Inverse Domain",
+        choice3: "Name Space",
+        choice4: "Country Domain",
+        answer: 3
+    },
+]
+
+const questions8 = [
+    {
+        title: "Root Server and Name Resolution",
+        question: "What is a root server?",
+        choice1: "DNS name servers that work in the root zone.",
+        choice2: "It is utilized for planning a location to a name.",
+        choice3: "In the realm of the Internet, the locations don't contain roads and urban areas; they have a numbers and images.",
+        choice4: "It characterizes the enlisted has as per their conventional behavior.",
+        answer: 1
+    },
+    {
+        title: "Root Server and Name Resolution",
+        question: "Root servers are fundamental piece ___________ of what",
+        choice1: "Server Domain",
+        choice2: "Framework of the internet",
+        choice3: "Realm of the internet",
+        choice4: "Queries",
+        answer: 2
+    },
+    {
+        title: "Root Server and Name Resolution",
+        question: "This is the realm of the Internet, the locations don't contain roads and urban areas; they have a numbers and images. What is it?",
+        choice1: "Server domain",
+        choice2: "Queries",
+        choice3: "Name resolution",
+        choice4: "Name space",
+        answer: 3
+    },
+    {
+        title: "Root Server and Name Resolution",
+        question: "What is a root server?",
+        choice1: "DNS name servers that work in the root zone.",
+        choice2: "It is utilized for planning a location to a name.",
+        choice3: "In the realm of the Internet, the locations don't contain roads and urban areas; they have a numbers and images.",
+        choice4: "It characterizes the enlisted has as per their conventional behavior.",
+        answer: 1
+    },
+    {
+        title: "Root Server and Name Resolution",
+        question: "What is TLD?",
+        choice1: "Troy Lee Designs",
+        choice2: "Third Level Domain",
+        choice3: "Top Level Domain",
+        choice4: "Technical Logistic Datas",
+        answer: 2
+    },
+    {
+        title: "Root Server and Name Resolution",
+        question: "What cycle where the program needs to get the IP and sends queries to the name servers?",
+        choice1: "Name space",
+        choice2: "Queries",
+        choice3: "Name resolution",
+        choice4: "Domain name resolver",
+        answer: 4
+    },
+]
+
+const questions9 = [
+    {
+        title: "DNS Security",
+        question: "TThis is an assault where fashioned Domain Name System information is brought into a Domain Name System resolver's cache, bringing about the resolver returning an erroneous IP address for a domain. What is the answer?",
+        choice1: "Domain Name System tunneling",
+        choice2: "Domain Name System hijacking",
+        choice3: "Domain Name System cache poisonings",
+        choice4: "NXDOMAIN",
+        answer: 3
+    },
+    {
+        title: "DNS Security",
+        question: "What is a phantom domain?",
+        choice1: "A comparative outcome to a NXDOMAIN assault on a DNS resolver.",
+        choice2: "Assailants organize this type of assault by setting up especially to domains and resolvers to make TCP associations with other authentic resolvers.",
+        choice3: "In Domain Name System commandeering the aggressor diverts inquiries to an alternate domain name server.",
+        choice4: "These assaults are completed utilizing CPE gadgets.",
+        answer: 1
+    },
+    {
+        title: "DNS Security",
+        question: "What domain attack is this assaults are completed utilizing CPE gadgets?",
+        choice1: "Domain lock-up",
+        choice2: "Random subdomain",
+        choice3: "Domain Name System hijacking",
+        choice4: "Botnet-based CP",
+        answer: 4
+    },
+    {
+        title: "Root Server and Name Resolution",
+        question: "What is a root server?",
+        choice1: "DNS name servers that work in the root zone.",
+        choice2: "It is utilized for planning a location to a name.",
+        choice3: "In the realm of the Internet, the locations don't contain roads and urban areas; they have a numbers and images.",
+        choice4: "It characterizes the enlisted has as per their conventional behavior.",
+        answer: 1
+    },
+    {
+        title: "DNS Security",
+        question: "This is a sort of Domain Name System flood assault where an assailant immerses a Domain Name System server with demands, requesting records that don't exist, trying to cause a refusal of administration for real traffic. What is the correct answer?",
+        choice1: "Random subdomain",
+        choice2: "Botnet-based CP",
+        choice3: "NXDOMAIN",
+        choice4: "Domain lock-up",
+        answer: 3
+    },
+    {
+        title: "DNS Security",
+        question: "How many Domain security we have?",
+        choice1: "8",
+        choice2: "6",
+        choice3: "10",
+        choice4: "5",
+        answer: 1
+    },
+]
+
+
+// {
+//     title: "DNS and History",
+//     question: "The paper on the most recent DNSSEC convention was distributed in _______. What year?",
+//     choice1: "2007",
+//     choice2: "2019",
+//     choice3: "2005",
+//     choice4: "2009",
+//     answer: 3
+// }
---
+++ js/components/quizcontroller.js
@@ -0,0 +1,94 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.answer'));
+const progressText = document.querySelector('#progress-text');
+const progressBar = document.querySelector('#progressbar');
+const congratsContainer = document.querySelector('#greet')
+const contentContainer = document.querySelector('.content')
+const scoreText = document.querySelector('#score-text')
+const choiceParent = document.querySelector('#choice-texts-parent')
+const resultContainer = document.querySelector('.result-container')
+const choicesChild = Array.from(choiceParent.children)
+
+let currentQuestion = {}
+let acceptingAnswer = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+const MAX_QUESTIONS = 8
+
+
+
+loadQuestion = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+
+getNewQuestion = () => {
+
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+        contentContainer.style.display = 'none'
+        scoreText.innerText = `${score} out of ${MAX_QUESTIONS}`
+        return congratsContainer.style.display = 'block'
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBar.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`
+
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionIndex]
+    question.innerHTML = currentQuestion.question
+
+    // if(currentQuestion.question.startsWith('Give')){
+
+    // }
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionIndex, 1)
+    acceptingAnswer = true
+
+    
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if (!acceptingAnswer) return
+
+        acceptingAnswer = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'answer-correct' : 'answer-wrong'
+
+        if (classToApply === 'answer-correct') {
+            score += 1
+        }
+
+        selectedChoice.classList.add(classToApply)
+        choiceParent.classList.add('disabled')
+
+        setTimeout(() => {
+            selectedChoice.classList.remove(classToApply)
+            choiceParent.classList.remove('disabled')
+            getNewQuestion()
+        }, 1000)
+    })
+})
+
+
+// giveExample = () => {
+//     choicesChild.forEach( choice => choice.style.display = 'none')
+//     choiceParent.innerHTML = ''
+// }
+
+
+window.onload = loadQuestion()
